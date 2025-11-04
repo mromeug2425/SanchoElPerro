bbc2127071b2959161e7cbdfb67991591ce51c12
ddelacortep <ddelacortep@gmail.com>
2025-11-04 17:17:51 +0100

    Creacion de los modelos y controladores

---
+++ backend/app/Http/Controllers/SesionesController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Sesiones;
+use Illuminate\Http\Request;
+
+class SesionesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Sesiones $sesiones)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Sesiones $sesiones)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Sesiones $sesiones)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Sesiones $sesiones)
+    {
+        //
+    }
+}
