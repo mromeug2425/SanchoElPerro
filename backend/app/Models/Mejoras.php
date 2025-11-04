1f0d8b0e915451857055f455aa23dec5993c6320
ddelacortep <ddelacortep@gmail.com>
2025-11-04 18:37:17 +0100

    creacion relaciones usuarios

--- backend/app/Models/Mejoras.php
+++ backend/app/Models/Mejoras.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Mejoras extends Model
 {
     //
+    protected $table = 'mejoras';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
 }
--- backend/app/Models/Sesiones.php
+++ backend/app/Models/Sesiones.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Sesiones extends Model
 {
     //
+    protected $table = 'sesiones';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
 }
