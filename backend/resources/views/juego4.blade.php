ad57a3348a7a70e203e6ba817615007604c5729a
mromeug2425 <mromeug2425@politecnics.barcelona>
2025-11-07 17:08:33 +0100

    initialize juego 4

--- backend/resources/views/juego4.blade.php
+++ backend/resources/views/juego4.blade.php
@@ -4,6 +4,8 @@
 
 @php
 	$backgroundImage = asset('img/backgrounds/joc4.png');
+	$maxLife = 100;
+	$currentLife = 20; // Change this value to see the bar change
 @endphp
 
 @section('content')
@@ -16,8 +18,51 @@
 			</a>
 		</div>
 
-		<main class="flex-1 flex items-center justify-center">
-			<div class="animate-fade-in-scale">
+		<main class="flex-1 flex items-start justify-center pt-20">
+			<div class="animate-fade-in-scale w-full">
+				<!-- Contenido del Juego 4 -->
+				<div class="text-center w-full justify-between flex items-center">
+					<div class="text-center flex gap-4">
+						<div class="text-center flex flex-col gap-4">
+							<x-boton color="blue" text="23" height="large" />
+							<x-boton text="2" height="large" />
+							<x-boton color="green" text="4" height="large" />
+							<x-boton color="red" text="5" height="large" />
+						</div>
+						<div class="text-center flex flex-col gap-4">
+							<x-boton color="blue" text="x" height="large" />
+							<x-boton text="+" height="large" />
+							<x-boton color="blue" text="-" height="large" />
+						</div>
+					</div>
+					<div class="text-center flex gap-4">
+						<div class="text-center flex flex-col gap-4">
+							<x-dialogo bg_color="#D9D9D9" border_color="#000000" text="2x14-5" />
+							<x-boton text="Submit" height="large" />
+						</div>
+						<div class="text-center flex flex-col gap-4">
+							<x-dialogo bg_color="#D9D9D9" border_color="#000000" text="" />
+						</div>
+						<div class="text-center flex flex-col gap-4">
+							<div class="flex flex-col items-center">
+								<div class="relative w-12 h-64 bg-gray-800 rounded-full border-4 border-black shadow-lg overflow-hidden">
+									<div class="absolute inset-0 bg-red-900/30"></div>
+									
+									<div class="absolute bottom-0 left-0 right-0 transition-all duration-300 ease-out"
+										 style="height: {{ ($currentLife / $maxLife) * 100 }}%;">
+										<div class="w-full h-full bg-red-600"></div>
+									</div>
+									
+									<div class="absolute inset-0 flex items-center justify-center z-10">
+										<span class="font-jersey text-white text-lg font-bold drop-shadow-lg">
+											{{ $currentLife }}
+										</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</main>
 	</div>
