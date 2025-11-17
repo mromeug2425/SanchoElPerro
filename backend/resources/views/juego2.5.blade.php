36b5dde0be75853a571c02ef9db72478ca5a89dd
ddelacortep <ddelacortep@gmail.com>
2025-11-17 17:01:49 +0100

    creacion de la rama

---
+++ backend/resources/views/juego2.5.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('title', config('app.name', 'SanchoElPerro') . ' - Juego 2.5')
+
+@php
+	$backgroundImage = asset('img/backgrounds/joc3.png');
+@endphp
+
+@section('content')
+
+
+
+
+@endsection
